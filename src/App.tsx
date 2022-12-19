09db13253b6d3645ad6e7120b1cf13aa21c7472a
J. Fernandes <j.fernandes8077@gmail.com>
2022-12-19 17:50:27 -0300

    feat: add typography components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { StyledText } from "./components/Text/style";
+import { Title } from "./components/Title";
+import { StyledTitle } from "./components/Title/style";
 
 function App() {
   return (
     <div>
       <h1>first commit</h1>
       <h2>first commit dev</h2>
+      <Title tag="h5">teste</Title>
+      <StyledTitle tag="h2" color="red" title="one">
+        teste estiloso
+      </StyledTitle>
+      <StyledText tag="span">span stiloso</StyledText>
     </div>
   );
 }
